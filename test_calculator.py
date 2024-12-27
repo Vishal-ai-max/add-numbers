b3c8482c67e65deb83e52572221e102dabc718a8
Vishal <vs608440@gmail.com>
2024-12-27 13:41:14 +0530

    newline instead of comma testcase

--- test_calculator.py
+++ test_calculator.py
@@ -12,5 +12,8 @@ class CalculatorTest(unittest.TestCase):
     def test_two_numbers(self):
         self.assertEqual(add("5,9"), 14)
 
+    def test_newline_instead_of_comma(self):
+        self.assertEqual(add("1\n2,3"), 6)
+
 if __name__ == '__main__':
     unittest.main()
