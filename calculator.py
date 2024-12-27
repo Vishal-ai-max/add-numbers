5e65c5116d7c4cca1fdf3eb4ece376ef7cb2638e
Vishal <vs608440@gmail.com>
2024-12-27 15:11:24 +0530

    negative testcase resolved

--- calculator.py
+++ calculator.py
@@ -6,4 +6,13 @@ def add(numbers: str):
         numbers = numbers[4:]      # slicing numbers to remove delimiter initialization.
     else:
         delimiter = ','
-    return sum(map(int, numbers.replace("\n", ",").split(delimiter)))
+    negative_numbers = []
+    int_numbers = []
+    for num in numbers.replace("\n", ",").split(delimiter):
+        int_numbers.append(int(num))
+    for num in int_numbers:
+        if num < 0:
+            negative_numbers.append(num)
+    if negative_numbers:
+        raise ValueError(f"negative numbers not allowed: {', '.join(map(str, negative_numbers))}")
+    return sum(int_numbers)
