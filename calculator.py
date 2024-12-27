0e400d2425424645b7909059831ff581421162d8
Vishal <vs608440@gmail.com>
2024-12-27 14:32:07 +0530

    custom delimiter testcase resolved

--- calculator.py
+++ calculator.py
@@ -1,4 +1,9 @@
 def add(numbers: str):
     if not numbers:
         return 0
-    return sum(map(int, numbers.replace("\n", ",").split(',')))
+    if numbers.startswith("//"):
+        delimiter = numbers[2]     # assuming delimiter will be of length one
+        numbers = numbers[4:]      # slicing numbers to remove delimiter initialization.
+    else:
+        delimiter = ','
+    return sum(map(int, numbers.replace("\n", ",").split(delimiter)))
